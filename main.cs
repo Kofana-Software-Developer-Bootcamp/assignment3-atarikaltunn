b5010030433d6c76312bcdae6f14b8cb3a8ea595
atarikaltunn <atarikaltunn@gmail.com>
2022-03-11 01:37:59 +0300

    There is a bug with Car type array initialize, welcome page added and fixed

--- main.cs
+++ main.cs
@@ -1,114 +1,125 @@
 using System;
 using System.IO;
 
-class Car //car class to car saling
+namespace Civciv
 {
-    private string brand;
-    private int year, kilometers, damageAmount;
-
-    // public functions to gets/sets car's informations
-    public string Brand
+    class Assignment
     {
-        get { return brand; }
-        set { brand = value; }
-    }
-    public int Year
-    {
-        get { return year; }
-        set { year = value; }
-    }
-    public int DamageAmount
-    {
-        get { return damageAmount; }
-        set { damageAmount = value; }
-    }
-    public int Kilometers
-    {
-        get { return kilometers; }
-        set { kilometers = value; }
-    }
+        class Car //car class to car saling
+        {
+            private string brand = "";
+            private int year = 0, kilometers = 0, damageAmount = 0;
 
-}
+            // public functions to gets/sets car's informations
+            public string Brand
+            {
+                get { return brand; }
+                set { brand = value; }
+            }
+            public int Year
+            {
+                get { return year; }
+                set { year = value; }
+            }
+            public int DamageAmount
+            {
+                get { return damageAmount; }
+                set { damageAmount = value; }
+            }
+            public int Kilometers
+            {
+                get { return kilometers; }
+                set { kilometers = value; }
+            }
 
-class Person //person class to user seller/buyer
-{
-    private int age;
-    private string name, identityNumber, phoneNum;
-    private Car[] carArray;
+        }
 
-    // public functions to gets/sets person's informations
+        class Person //person class to user seller/buyer
+        {
+            private int age = 0;
+            private string name = "", identityNumber = "", phoneNum = "";
+            //private Car[] carArray = new Car[] { };
 
-    public int Age
-    {
-        get { return age; }
-        set { age = value; }
-    }
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }
-    }
-    public string IdentityNumber
-    {
-        get { return identityNumber; }
-        set { identityNumber = value; }
-    }
-    public string PhoneNum
-    {
-        get { return phoneNum; }
-        set { phoneNum = value; }
-    }
-    public Car CarArray
-    {
-        get { return carArray; }
-        set { carArray = value; }
-    }
+            // public functions to gets/sets person's informations
 
-}
-class Assignment
-{
+            public int Age
+            {
+                get { return age; }
+                set { age = value; }
+            }
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public string IdentityNumber
+            {
+                get { return identityNumber; }
+                set { identityNumber = value; }
+            }
+            public string PhoneNum
+            {
+                get { return phoneNum; }
+                set { phoneNum = value; }
+            }
+            //public Car CarArray
+            //{
+            //    get { return carArray; }
+            //    set { carArray = value; }
+            //}
 
-    // Main Method
-    public static void Main()
-    {
+        }
+        // Main Method
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine(
+                "Welcome to car sale!\n" +
+                "------------------------------------------\n" +
+                "*To continue please Sign in (press 1).\n" +
+                "*Don't have an account? Sign up (press 2).\n" +
+                "*To exit, please press 'q'."
+                );
 
+                string statement = "";
 
-        Console.WriteLine(
-            "Welcome to car rent!" +
-            "To continue please Sign in (press 1)." +
-            "Don't have an account? Sign up (press 2)." +
-            "To exit, please press 'q'."
-            );
+                // while (true)
+                // {
 
-        string statement = "";
+                //     if (statement = Console.ReadLine == "q")
+                //     {
+                //         break;
+                //     }
 
-        // while (true)
-        // {
+                //     else if (statement = Console.ReadLine == "1")
+                //     {
 
-        //     if (statement = Console.ReadLine == "q")
-        //     {
-        //         break;
-        //     }
+                //     }
 
-        //     else if (statement = Console.ReadLine == "1")
-        //     {
-
-        //     }
-
-        //     else if (statement = Console.ReadLine == "2")
-        //     {
-        //         Console.WriteLine("Enter your name: ");
-        //         name = Console.ReadLine();
-
-
-        //         Console.WriteLine("Enter your age: ");
-        //         age = Convert.ToInt32(Console.ReadLine());
-
-        //         Console.WriteLine("Enter your Identity Number: ");
-        //         identityNumber = Console.ReadLine();
+                //     else if (statement = Console.ReadLine == "2")
+                //     {
+                //         Console.WriteLine("Enter your name: ");
+                //         name = Console.ReadLine();
 
 
-        //     }
-        // }
+                //         Console.WriteLine("Enter your age: ");
+                //         age = Convert.ToInt32(Console.ReadLine());
+
+                //         Console.WriteLine("Enter your Identity Number: ");
+                //         identityNumber = Console.ReadLine();
+
+
+                //     }
+                // }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+
+        }
     }
 }
